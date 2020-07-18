4eee7886606245327b24cd9acc7653f85ad02e39
Jinxing Lin <172601673@qq.com>
2020-07-18 23:28:38 +0800

    repl

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,59 @@
+import path from "path";
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import styles from "rollup-plugin-styles";
 
-export default [
-  {
-    input: "packages/common/index.ts",
-    plugins: [typescript(), styles(), commonjs(), resolve()],
-    output: [
-      {
-        file: "dist/bundle.js",
-        format: "iife",
-        sourcemap: true,
-      },
-    ],
-  },
-];
+const genPackageConfig = (pkg) => {
+  const plugins = [
+    commonjs(),
+    resolve(),
+    styles(),
+    typescript({ include: [/packages\/.*/] }),
+  ];
+
+  const input = path.resolve("packages", pkg, "index.ts");
+  const name = "$$" + pkg;
+
+  return [
+    // browser
+    {
+      input,
+      plugins,
+      output: [
+        {
+          name,
+          dir: path.resolve("dist", pkg),
+          format: "iife",
+          sourcemap: true,
+        },
+      ],
+    },
+    // commonjs
+    {
+      input,
+      plugins,
+      output: [
+        {
+          dir: path.resolve("dist/cjs", pkg),
+          format: "cjs",
+          sourcemap: true,
+        },
+      ],
+    },
+    // es module
+    {
+      input,
+      plugins,
+      output: [
+        {
+          dir: path.resolve("dist/es", pkg),
+          format: "esm",
+          sourcemap: true,
+        },
+      ],
+    },
+  ];
+};
+
+export default [...genPackageConfig("player"), ...genPackageConfig("record")];
