554942e4affe5af5f19adf510ad9e1b0ea6bc2a8
Jinxing Lin <172601673@qq.com>
2020-10-02 22:50:33 +0800

    update code

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+
+const config = {};
+
+export default config;
